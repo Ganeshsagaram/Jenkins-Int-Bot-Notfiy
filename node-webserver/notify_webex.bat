d6e7b0832168a2ccae65b9fdd5713cf3f63a23c6
GANESH SAGARAM <ganeshsai1504@gmail.com>
2025-06-16 16:39:18 +0530

    Change the token and room details, which were Hardcoded

--- node-webserver/notify_webex.bat
+++ node-webserver/notify_webex.bat
@@ -2,8 +2,8 @@
 setlocal
 
 REM Set your Webex credentials
-set WEBEX_TOKEN="Bearer ZjkyYzczM2YtYjM2ZS00OWRjLTgyYTktNWEwYjdjMjJjNThhZWQ4MTIyY2UtM2Fj_P0A1_719e5834-e6ce-4718-b3d0-7b6ed355fd8e"
-set ROOM_ID="Y2lzY29zcGFyazovL3VybjpURUFNOnVzLXdlc3QtMl9yL1JPT00vMzFmNDY1ZDAtMzYxYy0xMWYwLWE2NzYtZjEwZmVlYTA5NTMz"
+set WEBEX_TOKEN="Bearer <Token>"
+set ROOM_ID="ROOM_ID"
 
 REM Jenkins variables (available in freestyle jobs)
 set JOB_NAME=%JOB_NAME%
